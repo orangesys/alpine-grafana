4d138944d4a4c9f0b877b2c3e485344954cc71e9
gavinzhou <gavin.zhou@gmail.com>
2016-07-29 09:56:18 +0000

    first commit

---
+++ chechgrafana.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl -s 'http://admin:admin@127.0.0.1:3000/api/datasources' \
+    -X POST \
+    -H 'Content-Type: application/json;charset=UTF-8' \
+    --data-binary '{
+	"name":"localinflux",
+	"type":"influxdb",
+	"url":"http://influxsrv:8086",
+	"access":"proxy",
+	"isDefault":true,
+	"database":"telegraf",
+	"user":"root",
+	"password":"root"}'|jq
