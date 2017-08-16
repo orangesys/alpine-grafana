3126270786c51c0c564f31323c714d7e500629d2
gavinzhou <gavin.zhou@gmail.com>
2017-08-16 12:09:39 +0900

    fix mistake

---
+++ scripts/checkgrafana.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+version=$(git describe --always --tags|sed 's/^v//')
+
+docker run -d -p 3000:3000 orangesys/alpine-grafana:${version}
+
+curl -s 'http://admin:admin@127.0.0.1:3000/api/datasources' \
+    -X POST \
+    -H 'Content-Type: application/json;charset=UTF-8' \
+    --data-binary '{
+	"name":"localinflux",
+	"type":"influxdb",
+	"url":"http://influxsrv:8086",
+	"access":"proxy",
+	"isDefault":true,
+	"database":"telegraf",
+	"user":"root",
+	"password":"root"}'|grep -q "Datasource added"
