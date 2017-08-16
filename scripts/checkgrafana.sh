6b25993b540e3f4b8db933234bc1660344fd175c
gavinzhou <gavin.zhou@gmail.com>
2017-08-16 12:14:26 +0900

    test with debug

--- scripts/checkgrafana.sh
+++ scripts/checkgrafana.sh
@@ -4,7 +4,9 @@ set -ex
 version=$(git describe --always --tags|sed 's/^v//')
 
 docker run -d -p 3000:3000 orangesys/alpine-grafana:${version}
-
+sleep 3
+docker ps
+netstat -an|grep 3000
 curl -s 'http://admin:admin@127.0.0.1:3000/api/datasources' \
     -X POST \
     -H 'Content-Type: application/json;charset=UTF-8' \
