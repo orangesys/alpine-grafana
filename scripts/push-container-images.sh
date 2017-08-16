ff73f0489b4c7a5d75d6ff4f074b78eca359ac3c
gavinzhou <gavin.zhou@gmail.com>
2017-08-16 13:06:38 +0900

    tes ci 1

--- scripts/push-container-images.sh
+++ scripts/push-container-images.sh
@@ -5,11 +5,6 @@ version=$(git describe --always --tags|sed 's/^v//')
 
 docker push orangesys/${CIRCLE_PROJECT_REPONAME}
 
-echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/account-auth.json
-gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
-gcloud config set project $PROJECT_NAME
 docker tag "orangesys/${CIRCLE_PROJECT_REPONAME}:${version}" "asia.gcr.io/saas-orangesys-io/${CIRCLE_PROJECT_REPONAME}:${version}"
-gcloud docker -- push "asia.gcr.io/saas-orangesys-io/${CIRCLE_PROJECT_REPONAME}:${version}"
-
 docker images
-docker logout
+gcloud docker -- push "asia.gcr.io/saas-orangesys-io/${CIRCLE_PROJECT_REPONAME}:${version}"
