00aec8076159987e6bc5ba29255b3f37f2e05104
gavinzhou <gavin.zhou@gmail.com>
2017-08-16 12:59:42 +0900

    fix format

--- scripts/checkgrafana.sh
+++ scripts/checkgrafana.sh
@@ -5,19 +5,19 @@ version=$(git describe --always --tags|sed 's/^v//')
 
 docker run -d --name grafana -p 3000:3000 orangesys/alpine-grafana:${version}
 docker run --network container:grafana \
-  appropriate/curl -s -X GET 'http://127.0.0.1:3000/api/health'
+		appropriate/curl -s -X GET 'http://127.0.0.1:3000/api/health' | grep -q "ok"
 
 docker run --network container:grafana \
-  appropriate/curl \
+		appropriate/curl \
     -s 'http://admin:admin@127.0.0.1:3000/api/datasources' \
     -X POST \
     -H 'Content-Type: application/json;charset=UTF-8' \
     --data-binary '{
-	"name":"localinflux",
-	"type":"influxdb",
-	"url":"http://influxsrv:8086",
-	"access":"proxy",
-	"isDefault":true,
-	"database":"telegraf",
-	"user":"root",
-	"password":"root"}'|grep "Datasource added"
+		"name":"localinflux",
+		"type":"influxdb",
+		"url":"http://influxsrv:8086",
+		"access":"proxy",
+		"isDefault":true,
+		"database":"telegraf",
+		"user":"root",
+		"password":"root"}'|grep -q "Datasource added"
