5e09ef7646c797713188a28efdc276433ff5459c
gavinzhou <gavin.zhou@gmail.com>
2017-09-06 13:32:00 +0900

    test ci 1

--- scripts/create-container.sh
+++ scripts/create-container.sh
@@ -5,6 +5,3 @@ set -ex
 version=$(git describe --always --tags)
 docker build --tag "orangesys/${CIRCLE_PROJECT_REPONAME}:${version}" .
 docker images
-mkdir -p /caches
-docker save -o /caches/${CIRCLE_PROJECT_REPONAME}.tar "orangesys/${CIRCLE_PROJECT_REPONAME}:${version}"
-ls /caches/
