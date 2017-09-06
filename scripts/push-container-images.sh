d16853abf9345cb8e15f73ffc3a385ae439ef8f9
gavinzhou <gavin.zhou@gmail.com>
2017-09-06 12:50:14 +0900

    test ci 1

--- scripts/push-container-images.sh
+++ scripts/push-container-images.sh
@@ -3,8 +3,8 @@ set -ex
 
 version=$(git describe --always --tags)
 
-docker push orangesys/${CIRCLE_PROJECT_REPONAME}
+docker push orangesys/${CIRCLE_PROJECT_REPONAME}:${version}
 
-docker tag "orangesys/${CIRCLE_PROJECT_REPONAME}:${version}" "asia.gcr.io/saas-orangesys-io/${CIRCLE_PROJECT_REPONAME}:${version}"
+#docker tag "orangesys/${CIRCLE_PROJECT_REPONAME}:${version}" "asia.gcr.io/saas-orangesys-io/${CIRCLE_PROJECT_REPONAME}:${version}"
+#gcloud docker -- push "asia.gcr.io/saas-orangesys-io/${CIRCLE_PROJECT_REPONAME}:${version}"
 docker images
-gcloud docker -- push "asia.gcr.io/saas-orangesys-io/${CIRCLE_PROJECT_REPONAME}:${version}"
