64aa4b7c6b7c89cd07aab8e910f7eb465707c7a6
gavinzhou <gavin.zhou@gmail.com>
2018-05-31 18:54:11 +0900

    changed curl container image

--- .circleci/checkgrafana.sh
+++ .circleci/checkgrafana.sh
@@ -6,10 +6,10 @@ version=$(git describe --always --tags)
 docker run -d --name grafana -p 3000:3000 orangesys/alpine-grafana:${version}
 sleep 1
 docker run --network container:grafana \
-		byrnedo/alpine-curl -s -X GET 'http://127.0.0.1:3000/api/health'
+		orangesys/docker-curl -s -X GET 'http://127.0.0.1:3000/api/health'
 
 docker run --network container:grafana \
-		byrnedo/alpine-curl \
+		orangesys/docker-curl \
     -s 'http://admin:admin@127.0.0.1:3000/api/datasources' \
     -X POST \
     -H 'Content-Type: application/json;charset=UTF-8' \
