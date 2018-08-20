662349a641cd69bf3f640c5d7e3ef2b717ac4022
Tiago Augusto Pimenta <tiagoapimenta@users.noreply.github.com>
2018-08-20 15:44:43 -0300

    Simplify runner

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/dumb-init /bin/bash 
+#!/bin/sh
 set -e
 
-if [ ! -z ${GF_INSTALL_PLUGINS} ]; then
-  OLDIFS=$IFS
-  IFS=','
-  for plugin in ${GF_INSTALL_PLUGINS}; do
-    grafana-cli plugins install ${plugin}
-  done
-  IFS=$OLDIFS
+if [ -n "$GF_INSTALL_PLUGINS" ]; then
+	printf '%s' "$GF_INSTALL_PLUGINS" |
+		tr ',' '\n' |
+		while read -r plugin; do
+		grafana-cli plugins install ${plugin}
+	done
 fi
 
-exec gosu grafana grafana-server  \
-  --homepath=/grafana
+if [ "$(stat -c "%U:%G" /grafana/data)" != grafana:grafana ]; then
+	chown grafana:grafana /grafana/data
+fi
+
+exec su-exec grafana grafana-server --homepath=/grafana
